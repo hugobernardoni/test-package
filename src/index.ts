8897c772e99b96b94839fa339bd234738f1fd150
hugooliveira <hugobernardoni@hotmail.com>
2021-10-06 15:00:25 -0300

    [RELEASE] testing

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 function GetName(name: string) {
-    var response = `Hello ${name}. Seja bem vindo!`
+    var response = `Hello ${name}. Seja bem vindo ao teste de pacote!`
     console.log(response);
 
     return response;
